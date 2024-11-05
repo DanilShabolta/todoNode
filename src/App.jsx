bf5b0c9a06ddceff0732d98e1ed53bbfe1b1b8a5
danilshabolta <danilshabolta@gmail.com>
2024-11-05 18:09:50 +0700

    fix: fixed info, share buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ const TaskManager = () => {
     const [editTask, setEditTask] = useState(null);
     const [showNoTasksMessage, setShowNoTasksMessage] = useState(true);
     const [overlayVisible, setOverlayVisible] = useState(false);
-    const [confirmDeleteTask, setConfirmDeleteTask] = useState(null);
+    const [confirmDeleteTask, setConfirmDeleteTask] = useState(false);
     const [shareMenuVisible, setShareMenuVisible] = useState(false);
+    const [infoVisible, setInfoVisible] = useState(false);
+    const [activeTaskIndex, setActiveTaskIndex] = useState(null);
 
     useEffect(() => {
         loadTasks();
@@ -46,7 +48,7 @@ const TaskManager = () => {
         setTasks(updatedTasks);
         saveTaskToLocalStorage(updatedTasks);
         setShowNoTasksMessage(updatedTasks.length === 0);
-        setOverlayVisible(false);
+        setOverlayVisible(true);
         setConfirmDeleteTask(null);
     };
 
@@ -74,82 +76,113 @@ const TaskManager = () => {
         setOverlayVisible(true);
     };
 
-    const handleOverlayClick = () => {
-        setOverlayVisible(false);
+    const handleOverlayClick = () => {// сброс оверлея и выделенной задачи
         setShareMenuVisible(false);
         setConfirmDeleteTask(null);
+        setActiveTaskIndex(null);
+        setInfoVisible(false);
     };
 
     const toggleShareMenu = () => {
         setShareMenuVisible(!shareMenuVisible);
     };
 
+    const toggleInfo = (task) => {
+        setInfoVisible(!infoVisible);
+        setTaskText(task.text);
+        setTaskTitle(task.title);
+    };
+
     return (
         <div id="app">
-          <div id="task-form">
-            <div id="task-input-container">
-              <div id="task-input-text">
-                <input
-                    id="task-title"
-                    type="text"
-                    value={taskTitle}
-                    onChange={(e) => setTaskTitle(e.target.value)}
-                    placeholder="Task Title"
-                />
-                <input
-                    id="task-input"
-                    type="text"
-                    value={taskText}
-                    onChange={(e) => setTaskText(e.target.value)}
-                    placeholder="Task Description"
-                />
+            <div id="task-form">
+                <div id="task-input-container">
+                    <div id="task-input-text">
+                        <input
+                            id="task-title"
+                            type="text"
+                            value={taskTitle}
+                            onChange={(e) => setTaskTitle(e.target.value)}
+                            placeholder="Task Title"
+                        />
+                        <input
+                            id="task-input"
+                            type="text"
+                            value={taskText}
+                            onChange={(e) => setTaskText(e.target.value)}
+                            placeholder="Task Description"
+                        />
+                    </div>
+                    {editTask ? (  
+                        <button onClick={saveEditTask}>Save Edit</button>
+                    ) : (
+                        <div className="add-button-container">
+                            <button onClick={addTask}>+</button>
+                        </div>
+                    )}
                 </div>
-                {editTask ? (  
-                    <button onClick={saveEditTask}>Save Edit</button>
-                ) : (
-                  <div className="add-button-container">
-                    <button onClick={addTask}>+</button>
-                  </div>
-                )}
             </div>
-            </div>
-
 
             {showNoTasksMessage && <div id="no-tasks-message">No tasks</div>}
             <div id="task-list">
                 {tasks.map((task, index) => (
-                    <div key={index} className="task-container">
+                    <div 
+                        key={index} 
+                        className="task-container" 
+                        onClick={() => setActiveTaskIndex(activeTaskIndex === index ? null : index)}
+                    >
                         <div className="task-main">
                             <div id="task-container-text">
-                            <h3 class="task-title">{task.title}</h3>
-                            <p class="task-description">{task.text}</p>
+                                <h3 className="task-title">{task.title}</h3>
+                                <p className="task-description">{task.text}</ p>
                             </div>
-                            <button onClick={() => handleDeleteConfirmation(task)} class="delete-btn">X</button>
-                            <button onClick={() => startEditTask(task)}><img src="./src/assets/button edit.png"/></button>
-                            <button onClick={toggleShareMenu}><img src="./src/assets/button share.png"/></button>
+                            <button onClick={() => handleDeleteConfirmation(task)} className="delete-btn">X</button>
                         </div>
-                        {shareMenuVisible && (
-                            <div className="share-menu">
+                        
+                        {activeTaskIndex === index && (
+                            <div className="task-dropdown-menu">
+                                <button onClick={() => startEditTask(task)} className='edit-btn'><img src="./src/assets/button edit.png"/></button>
+                                <button onClick={toggleShareMenu}><img src="./src/assets/button share.png"/></button>
+                                <button onClick={() => toggleInfo(task)} className='info-btn'><img src="./src/assets/button info.png"/></button>
+                                </div>
+                        )}
+
+                        {confirmDeleteTask &&(
+                            <div className="overlay" onClick={handleOverlayClick}>
+                            <div className="confirm-delete" onClick={(e) => e.stopPropagation()}>
+                                <h3>Confirm Delete</h3>
+                                <p>Are you sure you want to delete this task?</p>
+                                <div className='delete-buttons'>
+                                <button onClick={() => removeTask(confirmDeleteTask)}>Yes</button>
+                                <button onClick={handleOverlayClick}>No</button>
+                                </div>
+                            </div>
+                        </div>
+                        )}
+
+                        {shareMenuVisible &&(
+                            <div className='overlay' onClick={handleOverlayClick}>
+                            <div className="share-menu" onClick={(e) => e.stopPropagation()}>
                                 <button class="share-copy"><img src="./src/assets/share/share copy.png" alt="Share Copy"/></button>
                                 <button class="share-vk"><img src="./src/assets/share/share vk.png" alt="Share VK"/></button>
                                 <button class="share-tg"><img src="./src/assets/share/share tg.svg" alt="Share TG"/></button>
                                 <button class="share-ws"><img src="./src/assets/share/share ws.svg" alt="Share WS"/></button>
                                 <button class="share-fb"><img src="./src/assets/share/share fb.svg" alt="Share FB"/></button>
                             </div>
+                            </div>
+                        )}
+
+                        {infoVisible &&(
+                            <div className='overlay' onClick={handleOverlayClick}>
+                                <div className='info-content' onClick={(e) => e.stopPropagation()}>
+                                    <h3>{taskTitle}</h3>
+                                    <p>{taskText}</p>
+                                    </div>
+                            </div>
                         )}
                     </div>
                 ))}
             </div>
-            {overlayVisible && (
-                <div className="overlay" onClick={handleOverlayClick}>
-                    <div className="confirm-delete">
-                        <h3>Confirm Delete</h3>
-                        <p>Are you sure you want to delete this task?</p>
-                        <button onClick={() => removeTask(confirmDeleteTask)}>Yes</button>
-                        <button onClick={handleOverlayClick}>No</button>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
