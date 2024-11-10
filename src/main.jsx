4de2d6f7d0ca95f85ab903891e1da311a9654681
danilshabolta <danilshabolta@gmail.com>
2024-11-11 03:42:02 +0700

    удалил мусорные файлы

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
-//import TaskManager from './App.jsx'
 import TaskManager from './components/TaskManager';
 
 
