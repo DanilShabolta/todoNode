d8c6116a37aa26247e6d55b938c2155306f92780
danilshabolta <danilshabolta@gmail.com>
2024-11-17 19:13:11 +0700

    add: создал класс task changed: в TaskList изменил ключ на id

--- src/components/ui/TaskForm.jsx
+++ src/components/ui/TaskForm.jsx
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
-import '../../style.css';
-
+import React, { useState } from "react";
+import Task from "../utils/Task";
+import "../../style.css";
 
 const TaskForm = ({ addTask }) => {
-    const [taskTitle, setTaskTitle] = useState('');
-    const [taskText, setTaskText] = useState('');
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskText, setTaskText] = useState("");
 
-    const handleAddTask = () => {
-        if (taskTitle.trim() && taskText.trim()) {
-            const newTask = { title: taskTitle, text: taskText };
-            addTask(newTask);
-            setTaskTitle('');
-            setTaskText('');
-        }
-    };
+  const handleAddTask = () => {
+    if (taskTitle.trim() && taskText.trim()) {
+      const newTask = new Task(taskTitle, taskText); // Создаем новый объект Task
+      addTask(newTask);
+      setTaskTitle("");
+      setTaskText("");
+    }
+  };
 
-    return (
-        <div id="task-form">
-            <div id="task-input-container">
-                <div className='task-input-text'>
-                <input
-                    type="text"
-                    value={taskTitle}
-                    onChange={(e) => setTaskTitle(e.target.value)}
-                    placeholder="Task Title"
-                />
-                <input
-                    type="text"
-                    value={taskText}
-                    onChange={(e) => setTaskText(e.target.value)}
-                    placeholder="Task Description"
-                />
-                </div>
-                <button onClick={handleAddTask} className='add-button-container'>+</button>
-            </div>
+  return (
+    <div id="task-form">
+      <div id="task-input-container">
+        <div className="task-input-text">
+          <input
+            type="text"
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
+            placeholder="Task Title"
+          />
+          <input
+            type="text"
+            value={taskText}
+            onChange={(e) => setTaskText(e.target.value)}
+            placeholder="Task Description"
+          />
         </div>
-    );
+        <button onClick={handleAddTask} className="add-button-container">
+          +
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TaskForm;
